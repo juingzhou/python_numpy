76232331a639cd4f0569474d23d91ed67bb4894c
“ <juingzhou@163.com>
2018-06-05 10:26:39 +0800

    juingzhou

--- panda_introduce.py
+++ panda_introduce.py
@@ -7,3 +7,10 @@
 # @Software: PyCharm
 import pandas as pd
 import numpy as np
+
+s = pd.Series([1, 3, 6, np.nan, 44, 1])
+print(s)
+dates = pd.date_range('20180604', periods=7)
+print(dates)
+df = pd.DataFrame(np.random.randn(7, 4), index=dates, columns=['a', 'b', 'c', 'd'])
+print(df)
